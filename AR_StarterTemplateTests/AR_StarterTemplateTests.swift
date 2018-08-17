41ed45e526578d5733ac55ae05dd32f950977a8a
Eric M. Baumel <ebaumel@softcode.com>
2018-08-17 19:43:21 -0400

    Initial Commit

---
+++ AR_StarterTemplateTests/AR_StarterTemplateTests.swift
@@ -0,0 +1,34 @@
+//
+//  AR_StarterTemplateTests.swift
+//  AR_StarterTemplateTests
+//
+//  Created by Eric M. Baumel on 8/17/18.
+//  Copyright © 2018 Softcode Systems. All rights reserved.
+//
+
+import XCTest
+@testable import AR_StarterTemplate
+
+class AR_StarterTemplateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
