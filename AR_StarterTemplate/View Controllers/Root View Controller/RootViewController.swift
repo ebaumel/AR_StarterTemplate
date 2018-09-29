569b23934237ba25f7e801bc96b8eb373a94b190
Eric M. Baumel <ebaumel@softcode.com>
2018-09-29 16:18:10 -0400

    Initial Commit

--- AR_StarterTemplate/View Controllers/Root View Controller/RootViewController.swift
+++ AR_StarterTemplate/View Controllers/Root View Controller/RootViewController.swift
@@ -10,61 +10,141 @@ import UIKit
 import SceneKit
 import ARKit
 
-class RootViewController: UIViewController, ARSCNViewDelegate {
+class RootViewController: UIViewController {
   
   // MARK: - Properties
+  
+  var trackingStatus: String = ""
   
   // MARK: - Outlets
   
   @IBOutlet var sceneView: ARSCNView!
+  @IBOutlet weak var statusLabel: UILabel!
+  @IBOutlet weak var startButton: UIButton!
   
   // MARK: - Actions
+  @IBAction func startButtonPressed(_ sender: Any) {
+    print("Start Button Pressed.")
+  }
   
   // MARK: - View Management
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Set the view's delegate
-    sceneView.delegate = self
-    
-    // Show statistics such as fps and timing information
-    sceneView.showsStatistics = true
-    
-    // Create a new scene
-    let scene = SCNScene(named: "art.scnassets/ship.scn")!
-    
-    // Set the scene to the view
-    sceneView.scene = scene
+    initSceneView()
+    initScene()
+    configureARSession()
+    statusLabel.text = NSLocalizedString("Starting App!", comment: "")
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    
-    // Create a session configuration
-    let configuration = ARWorldTrackingConfiguration()
-    
-    // Run the view's session
-    sceneView.session.run(configuration)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
   }
   
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
-    
-    // Pause the view's session
     sceneView.session.pause()
   }
   
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    print("DidReceiveMemoryWarning()")
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+  
   // MARK: - Initialization
+  
+  func initSceneView() {
+    // Set the view's delegate
+    sceneView.delegate = self
+    // Show statistics such as fps and timing information
+    sceneView.showsStatistics = true
+    // Debug options - Remove for Production
+    sceneView.debugOptions = [
+      ARSCNDebugOptions.showFeaturePoints,
+      ARSCNDebugOptions.showWorldOrigin,
+      ARSCNDebugOptions.showBoundingBoxes,
+      ARSCNDebugOptions.showWireframe
+    ]
+  }
+  
+  func initScene() {
+    
+  }
+  
+  func configureARSession() {
+    guard ARWorldTrackingConfiguration.isSupported else {
+      print("ARKit is not available on this device.")
+      return
+    }
+    
+    let configuration = ARWorldTrackingConfiguration()
+    configuration.worldAlignment = .gravity // or .gravityAndHeading
+    configuration.providesAudioData = false
+    // configuration.isLightEstimationEnabled = true
+    sceneView.session.run(configuration)
+  }
+  
+}
+
+// MARK: - ARSCNViewDelegate
+
+extension RootViewController: ARSCNViewDelegate {
   
   // MARK: - Scene Kit Management
   
   // MARK: - Session State Management
   
+  func session(_ session: ARSession,
+               cameraDidChangeTrackingState camera: ARCamera) {
+    switch camera.trackingState {
+      case .notAvailable:
+        trackingStatus = NSLocalizedString("Tracking: Not available!", comment: "")
+      case .normal:
+        trackingStatus = NSLocalizedString("Tracking Normally!", comment: "")
+      case .limited(let reason):
+        switch reason {
+          case .excessiveMotion:
+            trackingStatus = NSLocalizedString("Tracking: Limited due to insufficient motion!", comment: "")
+          case .insufficientFeatures:
+            trackingStatus = NSLocalizedString("Tracking: Limited due to insufficent features!", comment: "")
+          case .initializing:
+            trackingStatus = NSLocalizedString("Tracking: Initializing...", comment: "")
+          case .relocalizing:
+            trackingStatus = NSLocalizedString("Tracking: Relocalizing...", comment: "")
+      }
+    }
+  }
+  
   // MARK: - Session Error Management
   
-  // MARK: - ARSCNViewDelegate
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    // Present an error message to the user
+    trackingStatus = NSLocalizedString("AR Session Failed: \(error.localizedDescription)", comment: "")
+    // resetTracking()
+  }
   
+  func sessionWasInterrupted(_ session: ARSession) {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    trackingStatus = NSLocalizedString("AR Session Was Interrupted!", comment: "")
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    trackingStatus = NSLocalizedString("AR Session Interruption Ended", comment: "")
+    // resetTracking()
+  }
+  
+  // MARK: - Plane Management
+  
+
   /*
    // Override to create and configure nodes for anchors added to the view's session.
    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
@@ -73,19 +153,6 @@ class RootViewController: UIViewController, ARSCNViewDelegate {
    return node
    }
    */
+
   
-  func session(_ session: ARSession, didFailWithError error: Error) {
-    // Present an error message to the user
-    
-  }
-  
-  func sessionWasInterrupted(_ session: ARSession) {
-    // Inform the user that the session has been interrupted, for example, by presenting an overlay
-    
-  }
-  
-  func sessionInterruptionEnded(_ session: ARSession) {
-    // Reset tracking and/or remove existing anchors if consistent tracking is required
-    
-  }
 }
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Starter Template for ARKit Projects
 
+Compass Directions:
+North: z-, East: x+, South: z+, West: x-
+
+Sources:
+XCode Augmented Reality App starter template
+ARKit By Tutorials by RayWenderlich.com team
+Apple SwiftShot demo app
